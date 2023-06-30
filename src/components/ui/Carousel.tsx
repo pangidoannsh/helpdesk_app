abf45f6a279586842afcf0b8749ab5aee3cf00ab
pangidoannsh <pangidoanhrp13@gmail.com>
2023-06-30 15:00:14 +0700

    fix:home

---
+++ src/components/ui/Carousel.tsx
@@ -0,0 +1,84 @@
+import { Icon } from "@iconify/react"
+import { useEffect, useState } from "react"
+
+const slides = [
+    {
+        image: "/assets/images/create.png",
+        title: "BUAT TIKET",
+        desc: "Buat tiket anda untuk melakukan pelaporan gangguan/permintaan teknologi"
+    },
+    {
+        image: "/assets/images/processing.png",
+        title: "TIKET DIPROSES",
+        desc: "Menunggu laporan gangguan/permintaan diproses oleh agen"
+    },
+    {
+        image: "/assets/images/feedback.png",
+        title: "BERI FEEDBACK",
+        desc: "Memberikan feedback dari tiket yang sudah selesai"
+    },
+]
+export default function Carousel() {
+    const [slidePos, setSlidePos] = useState(0);
+    const [indexSlide, setIndexSlide] = useState(0);
+
+    function handleSlider(action: string) {
+        if (action === "next") {
+            if (indexSlide === 2) {
+                setSlidePos(0);
+                setIndexSlide(0);
+            } else {
+                setSlidePos(prev => prev - 640);
+                setIndexSlide(prev => prev + 1);
+            }
+        } else {
+            if (indexSlide === 0) {
+                setSlidePos(-2 * 640);
+                setIndexSlide(2);
+            } else {
+                setSlidePos(prev => prev + 640);
+                setIndexSlide(prev => prev - 1);
+            }
+        }
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // console.log(indexSlide);
+
+            if (indexSlide === 2) {
+                setSlidePos(0);
+                setIndexSlide(0);
+            } else {
+                setSlidePos(prev => prev - 640);
+                setIndexSlide(prev => prev + 1);
+            }
+        }, 3000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [indexSlide])
+
+    return (
+        <div className="lg:w-[640px] overflow-hidden hidden lg:block relative -translate-y-4">
+            <div className="flex w-max duration-300" style={{ translate: `${slidePos}px 0` }}>
+                {slides.map((slide, index: number) => (
+                    <div key={index} className="lg:w-[640px] flex flex-col text-center">
+                        <div>
+                            <img src={slide.image} alt="buat tiket" className="lg:h-[392px] mx-auto" />
+                        </div>
+                        <div className="font-medium text-primary-600 text-4xl">{slide.title}</div>
+                        <div className="text-slate-600 font-open-sans text-xl">{slide.desc}</div>
+                    </div>
+                ))}
+            </div>
+            <button className="absolute w-max left-4 duration-100 top-1/2 -translate-y-1/2 text-5xl text-slate-400 hover:text-slate-500">
+                <Icon icon="octicon:chevron-left-16" onClick={() => handleSlider("prev")} />
+            </button>
+            <button className="absolute w-max right-4 duration-100 top-1/2 -translate-y-1/2 text-5xl text-slate-400 hover:text-slate-500">
+                <Icon icon="octicon:chevron-right-16" onClick={() => handleSlider("next")} />
+            </button>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,11 @@ import Modal from "@/components/ui/Modal";
 import CreateTicket from "@/components/common/CreateTicket";
 import CommonLayout from "@/components/layouts/Common"
 import { Icon } from '@iconify/react';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Footer from "@/components/common/Footer";
 import Link from "next/link";
 import Image from "next/image"
+import Carousel from "@/components/ui/Carousel";
 
 export default function Home() {
   const [isOpenCreateTiket, setIsOpenCreateTiket] = useState(false);
@@ -22,21 +23,22 @@ export default function Home() {
 
   return (
     <CommonLayout title="Helpdesk IT BPS Riau" >
-      <div className="relative px-6 sm:px-12 py-12 lg:py-24 xl:py-32 overflow-hidden">
-        <div className="flex flex-col gap-6 lg:w-[470px] lg:text-start text-center">
+      <div className="relative px-6 sm:px-12 mt-8 flex justify-between items-center">
+        <div className="flex flex-col gap-8 lg:w-[710px] lg:text-start text-center">
           <div className="flex flex-col gap-3">
             <h1 className="text-primary-800 text-6xl sm:text-8xl font-medium">HELPDESK</h1>
-            <h3 className="text-primary-500 text-4xl">BPS Riau</h3>
+            <h3 className="text-primary-600 text-4xl lg:text-6xl">BPS Riau</h3>
           </div>
           <p className="text-slate-600 text-xl md:text-2xl font-open-sans">
-            System pusat bantuan BPS Riau untuk memudahkan pelaporan dan pengolahan dokumentasi bantuan
+            Sistem pusat bantuan BPS Riau untuk memudahkan pelaporan gangguan/permintaan teknologi di BPS Riau dan
+            pengolahan dokumentasi bantuan IT Support
           </p>
           <div className="flex justify-center lg:hidden">
             <Icon icon="mdi:customer-service" className="text-9xl text-primary-600" />
           </div>
-          <button className="px-9 py-3 uppercase rounded-lg text-white bg-primary-500 hover:bg-primary-600 active:bg-primary-700
-          focus:ring-4 focus:ring-sky-200 text-xl md:text-2xl" onClick={() => setIsOpenCreateTiket(true)}>
-            pesan bantuan
+          <button className="px-9 py-3 lg:py-4 uppercase rounded-lg text-white bg-primary-500 hover:bg-primary-600 active:bg-primary-700
+          focus:ring-4 focus:ring-sky-200 text-xl md:text-2xl lg:w-max font-medium" onClick={() => setIsOpenCreateTiket(true)}>
+            buat tiket sekarang
           </button>
           <Link href='/ticket' className="lg:hidden block border rounded-md border-primary-500 text-primary-500 py-2 text-xl
           font-medium uppercase">
@@ -45,9 +47,10 @@ export default function Home() {
           <div>
           </div>
         </div>
-        <div className="absolute right-12 top-16 -z-10">
+        <Carousel />
+        {/* <div className="absolute right-12 top-16 -z-10">
           <img src="/assets/images/cs_image.png" alt="helpdesk illustration" className="hidden lg:block lg:w-[65vw] opacity-60 xl:opacity-100" />
-        </div>
+        </div> */}
       </div>
       <Footer />
       <Modal isOpen={isOpenCreateTiket} setIsOpen={setIsOpenCreateTiket} title="Pesan Layanan Bantuan" rounded="rounded-lg">
